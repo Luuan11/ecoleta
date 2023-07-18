55ae9f4efa5ed2fa9b091d83a212b3e844cd74ec
Luan Fernando <luancraft55@gmail.com>
2023-07-18 16:57:44 -0300

    :lipstick: created the folder partials and include to the pages

--- src/server.js
+++ src/server.js
@@ -4,15 +4,27 @@ const server = express()
 //configurando pasta public
 server.use(express.static("public"))
 
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views/", {
+    express: server,
+    noCache: true
+})
+
 // Pagina Inciial
 // Req: Requisição
 // Res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 })
 
+// Página de criação de pontos
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 //ligando o servidor
