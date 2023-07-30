3c0d640cdae259aa8734b7a5c9767d3ebc636d92
Luan Fernando <luancraft55@gmail.com>
2023-07-29 22:00:14 -0300

    :sparkles: starting the database

--- src/database/db.js
+++ src/database/db.js
@@ -12,6 +12,7 @@ db.serialize(() => {
         CREATE TABLE IF NOT EXISTS places (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             image TEXT,
+            name TEXT,
             address TEXT,
             address2 TEXT,
             state TEXT,
@@ -19,4 +20,46 @@ db.serialize(() => {
             items TEXT
         );
     `)
+
+    //inserindo dados a tabela
+    const query = 
+    `   INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?); `
+    db.run(query)
+
+    const values = [
+        "https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1613&q=80",
+        "Colectoria",
+        "Guilherme Gemballa, Jardim América",
+        "Número 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Resíduos Eletrônicos, Lâmpadas"
+    ]
+
+    function afterInsetData(err){
+        if(err){
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso!")
+        console.log(this)
+    }
+
+    db.all(`SELECT * FROM places`, function(err, rows){
+        if(err){
+            return console.log(err)
+        }
+
+        console.log("Aqui estão seus ")
+    })
+
+    db.run(query, values, afterInsetData)
 })
