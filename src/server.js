49b494cfe7d2e3f85de25982f9fc11310d0b5b3b
Luan Fernando <luancraft55@gmail.com>
2023-08-08 22:54:01 -0300

    :sparkles: configuring the page for the point created

--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const db = require("./database/db")
 //configurando pasta public
 server.use(express.static("public"))
 
+//habilitar o uso do req.body
+server.use(express.urlencoded({ extended: true }))
+
 //utilizando template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views/", {
@@ -14,11 +17,11 @@ nunjucks.configure("src/views/", {
     noCache: true
 })
 
-// Pagina Inciial
+// Pagina Inicial
 // Req: Requisição
 // Res: Resposta
 server.get("/", (req, res) => {
-    return res.render("index.html")
+    return res.render("index.html", {title : "Um titulo"})
 })
 
 // Página de criação de pontos
@@ -30,7 +33,40 @@ server.get("/create-point", (req, res) => {
 })
 
 server.post("/savepoint", (req, res) => {
-    return res.send("ok")
+
+    const query = 
+    `   INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?); `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsetData(err){
+        if(err){
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso!")
+        console.log(this)
+    }
+
+    db.run(query, values, afterInsetData)
+
+    return res.render("create-point.html", {saved: true })
 })
 
 server.get("/search", (req, res) => {
